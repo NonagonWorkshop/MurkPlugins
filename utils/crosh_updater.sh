fbd60d7adf17c645bad15643b7631742fad0fa19
StarkMist111960 <starkmist111960@gmail.com>
2025-10-30 10:58:16 -0600

    Update crosh_updater.sh

--- utils/crosh_updater.sh
+++ utils/crosh_updater.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+doas() {
+    ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
+}
+
+
 cd /
 cd mnt/stateful_partition/
-sudo mkdir murkmod/mushm/
+doas sudo mkdir murkmod/mushm/
 cd murkmod/mushm
 curl -O https://raw.githubusercontent.com/NonagonWorkshop/MurkPlugins/main/utils/mushm.sh
 cd /
