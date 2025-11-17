e4f3a06d4bc02b834d3e8bca30c108c2b55e8712
Star_destroyer12 <167476043+Star-destroyer12@users.noreply.github.com>
2025-11-17 11:52:09 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -19,9 +19,6 @@ sleep 1
 CROSH="/usr/bin/crosh"
 MURK_DIR="/mnt/stateful_partition/murkmod"
 MUSHM_URL="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/mushm.sh"
-BOOTMSG_URL="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/bootmsg.sh"
-START_BOOT_URL="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/nonamod.conf"
-
 
 log "Creating directories..."
 mkdir -p "$MURK_DIR/mush"
@@ -30,18 +27,6 @@ sleep 1
 
 log "Installing MushM"
 curl -fsSLo "$CROSH" "$MUSHM_URL" || error "Failed to download MushM"
-sleep 1
-
-Log "Fixing Weard Boot Message"
-curl -fsSL "$BOOTMSG_URL" || error "Failed to download Boot Message Fixer"
-cp bootmsg.sh /usr/local/bin/
-chmod +x /usr/local/bin/bootmsg.sh
-sleep 1
-
-log "Adding Boot Script"
-curl -fsSL "$START_BOOT_URL" || error "Failed to download Boot Script"
-cp nonamod.conf /etc/init/
-touch /var/log/nonamod.log
 sleep 1
 
 log "Installation complete!"
